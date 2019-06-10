bd6c2f398befb96b7b5a112f11e94e02b31752a2
root <md98765@naver.com>
2019-06-10 19:45:55 +0900

    pass puzzle, but timer service

--- [HW4]20151588/src/com/example/HW4_20151588/MainActivity2.java
+++ [HW4]20151588/src/com/example/HW4_20151588/MainActivity2.java
@@ -15,34 +15,191 @@ import android.widget.TextView;
 
 public class MainActivity2 extends Activity{
 	
-	LinearLayout linear;
+	// components
+	LinearLayout linear, content;
 	EditText data;
 	TextView tv;
 	Button btn;
-	OnClickListener ltn;
 	Button btns[];
-	LinearLayout linears[];
+	LinearLayout lns[];
 	
+	// global variables
 	int row,col,size;
 	boolean click_flag = true;
 	
-	public void shuffle(int num[], int size){
-		int i;
-		int a,b,tmp;
+	// "make buttons" OnClickListener
+	OnClickListener ltn = new OnClickListener(){
+		public void onClick(View v){
+			if(click_flag){
+				// get "row col"
+				String temp=data.getText().toString();
+				String t[] = temp.split(" ",2);
+				row = Integer.parseInt(t[0]);
+				col = Integer.parseInt(t[1]);
+				// check the size
+				if(row>5||col>5){
+					tv.setText("row&col should be less than 5");
+					return;
+				}
+				click_flag = false;
+				int num[] = new int[row*col];
+				
+				shuffle(num,row,col);
+				make_button(num,row,col);		
+			}
+		}
+	};
+	
+	// "puzzle buttons" OnClickListener
+	OnClickListener b_ltn = new OnClickListener(){
+		public void onClick(View v){
+			int i = v.getId();
+			if((i%col)!=0){	// check left
+				if(btns[i-1].getText().toString().equals("")){
+					remove_button();
+					swap_button(i,i-1);
+					set_button();
+					check_puzzle();
+				}
+			}
+			if((i%col)!=col-1){	// check right
+				if(btns[i+1].getText().toString().equals("")){
+					remove_button();
+					swap_button(i,i+1);
+					set_button();
+					check_puzzle();
+				}
+			}
+			if((i/col)!=0){	// check up
+				if(btns[i-col].getText().toString().equals("")){
+					remove_button();
+					swap_button(i,i-col);
+					set_button();
+					check_puzzle();
+				}
+			}
+			if((i/col)!=row-1){	// check down
+				if(btns[i+col].getText().toString().equals("")){
+					remove_button();
+					swap_button(i,i+col);
+					set_button();
+					check_puzzle();
+				}
+			}
+		}
+	};
+	
+	// shuffle the number 0 ~ row*col
+	public void shuffle(int num[], int row, int col){
+		int i,j,dir,tmp;
+		// using random seed
 		Random random = new Random();
-		for(i=0;i<size;i++) num[i] = i;
-		for(i=0;i<size;i++){
-			a = random.nextInt(size);
-			b = random.nextInt(size);
-			tmp = num[a];
-			num[a] = num[b];
-			num[b] = tmp;
+		for(i=0;i<row*col-1;i++) num[i]=i+1;
+		num[row*col-1] = 0;
+		for(i=0;i<10000;i++){
+			dir = random.nextInt(4);
+			for(j=0;num[j]!=0;j++);
+			//tv.setText(""+j);
+			switch(dir){
+			case 0:	// left
+				if(j%col != 0){
+					tmp = num[j];
+					num[j] = num[j-1];
+					num[j-1] = tmp;
+				}
+				break;
+			case 1:	// right
+				if(j%col != col-1){
+					tmp = num[j];
+					num[j] = num[j+1];
+					num[j+1] = tmp;
+				}
+				break;
+			case 2:	// up
+				if(j/col != 0){
+					tmp = num[j];
+					num[j] = num[j-col];
+					num[j-col] = tmp;
+				}
+				break;
+			case 3:	// down
+				if(j/col != row-1){
+					tmp = num[j];
+					num[j] = num[j+col];
+					num[j+col] = tmp;
+				}
+				break;
+			}
 		}
 	}
-	public void make_button(int num[], int row, int col){
+	
+	// set all buttons
+	public void set_button(){
+		int i,j;
+		for(i=0;i<row;i++){
+			for(j=0;j<col;j++){
+				lns[i].addView(btns[i*col+j]); 
+			}
+		}
+	}
+	
+	// swap two buttons
+	public void swap_button(int a, int b){
+		btns[b].setText(btns[a].getText());
+		btns[b].setBackgroundResource(android.R.drawable.btn_default);
+		btns[a].setText("");
+		btns[a].setBackgroundColor(Color.BLACK);
+	}
+	
+	// remove all buttons
+	public void remove_button(){
+		int i,j;
+		for(i=0;i<row;i++){
+			for(j=0;j<col;j++){
+				lns[i].removeView(btns[i*col+j]);
+			}
+		}
+	}
+	
+	// check whether puzzle is solved or not
+	public void check_puzzle(){
 		int i;
+		boolean pass = true;
+		for(i=0;i<row*col-1;i++){
+			if(!btns[i].getText().equals(Integer.toString(i+1))){
+				pass = false;
+				break;
+			}
+		}
+		if(pass) finish();
+	}
+	
+	// make row*col buttons
+	public void make_button(int num[], int row, int col){
+		int i,j;
 		btns = new Button[row*col];
-		//todo
+		lns = new LinearLayout[row];
+		for(i=0;i<row*col;i++) btns[i] = new Button(this);
+		for(i=0;i<row;i++){
+			lns[i] = new LinearLayout(this);
+			lns[i].setOrientation(LinearLayout.HORIZONTAL);
+			content.addView(lns[i]);
+		}
+		
+		for(i=0;i<row*col;i++){
+			// if num[i] then it will be blank button
+			if(num[i] == 0){
+				btns[i].setText("");
+				btns[i].setBackgroundColor(Color.BLACK);
+			}
+			else btns[i].setText(""+num[i]);
+			btns[i].setId(i);
+			btns[i].setOnClickListener(b_ltn);
+			btns[i].setWidth(1024/col);
+			btns[i].setHeight(360/row);
+		}
+		
+		set_button();
 	}
 	
 		@Override
@@ -51,40 +208,13 @@ public class MainActivity2 extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main2);
 		linear = (LinearLayout)findViewById(R.id.container);
+		content=(LinearLayout)findViewById(R.id.content);
 		data = (EditText)findViewById(R.id.editText1);
 		tv=(TextView)findViewById(R.id.textView1);
-		Button btn=(Button)findViewById(R.id.button1);
-		
-		ltn=new OnClickListener(){
-			public void onClick(View v){
-				if(click_flag){
-					String temp=data.getText().toString();
-					String t[] = temp.split(" ",2);
-					row = Integer.parseInt(t[0]);
-					col = Integer.parseInt(t[1]);
-					if(row>5||col>5){
-						tv.setText("row&col should be less than 5");
-						return;
-					}
-					click_flag = false;
-					size = row*col;
-					int num[] = new int[size];
-					
-					shuffle(num,size);
-					make_button(num,row,col);
-					
-					/*********/
-					temp = Integer.toString(num[0]);
-					for(int i = 1; i < size; i++){
-						temp = temp+" "+Integer.toString(num[i]);
-					}
-					tv.setText(temp);
-					/**********/
-					
-				}
-	
-			}
-		};
+		tv.setTextSize(30);
+		tv.setTextColor(Color.WHITE);
+		tv.setBackgroundColor(Color.BLACK);
+		btn=(Button)findViewById(R.id.button1);
 		btn.setOnClickListener(ltn);
 		
 	}
